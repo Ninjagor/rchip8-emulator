5ef44f29fedc961c4594d26802cc9be11dee7ccc
Rohit Karthik <rohitismaster@gmail.com>
2024-11-13 21:19:00 -0500

    finished all opcodes

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -57,6 +57,20 @@ fn main() {
                 } => {
                     break 'gameloop;
                 }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key_btn_mapper(key) {
+                        chip8.keypress(k, true);
+                    }
+                }
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key_btn_mapper(key) {
+                        chip8.keypress(k, false);
+                    }
+                }
                 _ => (),
             }
         }
@@ -66,6 +80,15 @@ fn main() {
         }
         chip8.tick_timers();
         draw_screen(&chip8, &mut canvas);
+
+        // print!("{}[2J", 27 as char);
+        // for k in 0..chip8.keyboard.len() {
+        //     if chip8.keyboard[k] == true {
+        //         println!("1 ");
+        //     } else {
+        //         print!("0 ");
+        //     }
+        // }
     }
 }
 
@@ -85,4 +108,26 @@ fn draw_screen(emu: &chip8::chip8::Chip8, canvas: &mut Canvas<Window>) {
         }
     }
     canvas.present();
+}
+
+fn key_btn_mapper(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
